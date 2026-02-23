ca8c837a6dc4c040541525841b1100b680fa71c0
Owen Parks <owenp1@uw.edu>
2026-02-23 00:15:44 -0800

    update sol. problem 0

--- projeuler0.py
+++ projeuler0.py
@@ -1,35 +1,5 @@
-import pandas
-import numpy
-import sympy
-
-
 integers = list(range(986000))
-'''
-test_integers = list(range(5))
 
-print(test_integers)
-
-test_squares = []
-
-for item in test_integers:
-    test_squares.append(item ** 2)
-
-print(test_squares)
-
-test_total = 0
-
-del test_squares[0]
-
-print(test_squares)
-
-for item in test_squares:
-    if item % 2 == 0:
-        pass
-    else:
-        test_total += item
-
-print(test_total)
-'''
 squares = []
 
 integers = list(range(986000))
